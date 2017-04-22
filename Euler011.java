4206c6ccf6e20b4d50357327903e163c7b4276b9
Sean Brede <seanbrede@gmail.com>
2017-04-22 16:08:50 -0700

    finished first edition of 11

--- Euler011.java
+++ Euler011.java
@@ -11,32 +11,50 @@ import java.util.List;
 public class Euler011 {
     
     public static void main(String[] args) {
-         String filename = JOptionPane.showInputDialog("Input the full path " +
-                "to a text file containing a pyramid of numbers:");
+        
+        String filename = JOptionPane.showInputDialog("Input the full path " +
+                "to a text file containing a grid of numbers:");
         
         List<String> gridList = EulerHelpers.buildListFromFile(filename);        
         int[][] gridArray = EulerHelpers.build2dArrayFromList(gridList);
         
-        int max = 0;
+        int max = 0, possibleMax;
         
-        for(int i = 0; i < gridArray.length; i++) {
-            for(int j = 0; j < gridArray[i].length; j++) {
-                if(j >= 3) {
-                    int possibleMax = gridArray[i][j] * gridArray[i][j - 1] *
-                            gridArray[i][j - 2] * gridArray[i][j - 3];
+        for(int y = 0; y < gridArray.length; y++) {
+            for(int x = 0; x < gridArray[y].length; x++) {
+                
+                // check horizontal
+                if(x > 2) {
+                    possibleMax = gridArray[y][x] * gridArray[y][x - 1] *
+                        gridArray[y][x - 2] * gridArray[y][x - 3];
                     if (possibleMax > max) max = possibleMax;
                 }
                 
-                if(i >= 3) {
+                // check vertical
+                if(y > 2) { 
+                    possibleMax = gridArray[y][x] * gridArray[y-1][x] *
+                        gridArray[y-2][x] * gridArray[y-3][x];
+                    if(possibleMax > max) max = possibleMax;
+                }
+                
+                // check up diagonal
+                if(y > 2 && x > 2) {
+                    possibleMax = gridArray[y][x] * gridArray[y-1][x-1] *
+                        gridArray[y-2][x-2] * gridArray[y-3][x-3];
+                    if(possibleMax > max) max = possibleMax;
                     
                 }
                 
-                if(j >= 3 && i >= 3) {
-                    
+                // check down diagonal
+                if(y < gridArray.length - 3 && x > 2) {
+                    possibleMax = gridArray[y][x] * gridArray[y+1][x-1] * 
+                        gridArray[y+2][x-2] * gridArray[y+3][x-3];
+                    if(possibleMax > max) max = possibleMax;
                 }
-                
-                // TODO
-            }
+            }         
         }
+        
+        System.out.println("The largest product of four adjacent cells " +
+            "in that grid is: " + max);
     }
 }
